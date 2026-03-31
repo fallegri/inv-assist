ce36c7e6ceef34e877fb53a340a5bee97b7a7538
fernandoallegri <129702397+fernandoallegri@users.noreply.github.com>
2026-03-30 22:46:29 -0400

    Enable database SSL

--- webapp/src/lib/db/index.ts
+++ webapp/src/lib/db/index.ts
@@ -8,8 +8,8 @@ export const pool =
   globalForPg.pool ||
   new Pool({
     connectionString: process.env.DATABASE_URL,
-    // Aquí puedes configurar SSL si al hacer deploy en Supabase/Neon lo requiere:
-    // ssl: { rejectUnauthorized: false }
+    // Requerido por Neon y Supabase en entornos de nube:
+    ssl: { rejectUnauthorized: false }
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPg.pool = pool;
